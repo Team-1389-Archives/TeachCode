13c71a9112589fc65caa3f641691760c2cd5174e
amindell11 <amindell11@gmail.com>
2016-10-11 10:36:19 -0400

    added input capability

--- SlickLibSetup/src/layout/IOHardware.java
+++ SlickLibSetup/src/layout/IOHardware.java
@@ -1,9 +1,12 @@
 package layout;
 
+import org.newdawn.slick.Input;
+
 import sprite.Sprite;
 
 public class IOHardware extends IOLayout{
 	public IOHardware(){
 		robot=new Sprite("res/Robot.png");
+		input=new Input(0);
 	}
 }
--- SlickLibSetup/src/layout/IOLayout.java
+++ SlickLibSetup/src/layout/IOLayout.java
@@ -1,7 +1,10 @@
 package layout;
 
+import org.newdawn.slick.Input;
+
 import sprite.Sprite;
 
 public class IOLayout {
 	public Sprite robot;
+	public Input input;
 }
