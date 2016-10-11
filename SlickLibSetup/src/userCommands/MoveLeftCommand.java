0d055ee4db58790c1a2900c52e3fdc30c0e65fa7
Jonathan <Jonathan@10.208.65.246>
2016-10-11 16:11:45 -0400

    Made my branch

--- SlickLibSetup/src/userCommands/MoveLeftCommand.java
+++ SlickLibSetup/src/userCommands/MoveLeftCommand.java
@@ -1,16 +1,22 @@
-package userCommands;
-
+package userCommands; // folder name
 import commands.Command;
+import sprite.Sprite; 
+public class MoveLeftCommand extends Command{ // name of the class
+	
 
-/** make the sprite move to the left from where it is
- *
- */
-public class MoveLeftCommand extends Command{
-
-	@Override
-	protected boolean execute() {
-		// TODO Auto-generated method stub
-		return false;
+		private Sprite controlledSprite; // making a new variable called controlledSprite
+		private int spaces; 
+		public MoveLeftCommand(Sprite controlledSprite, int spaces){
+			this.controlledSprite=controlledSprite;
+			this.spaces=spaces;
+		}
+		@Override
+		public boolean execute() {
+			controlledSprite.setX(controlledSprite.getX()-1);
+		
+			return true;
+		}
+		
 	}
 
-}
+
--- SlickLibSetup/src/userProgram/UserProgram.java
+++ SlickLibSetup/src/userProgram/UserProgram.java
@@ -2,11 +2,14 @@ package userProgram;
 
 import commands.Command;
 import commands.CommandUtils;
+import commands.DoNothingCommand;
 import commands.MonitorPositionCommand;
 import commands.MoveRightCommand;
 import commands.WaitTimeCommand;
 import layout.IOLayout;
 import programming.Program;
+import userCommands.MoveLeftCommand;
+import userCommands.MoveUpCommand;
 
 public class UserProgram extends Program{
 	public UserProgram(IOLayout io) {
@@ -16,15 +19,23 @@ public class UserProgram extends Program{
 	@Override
 	public Command provideCommand() {
 		return CommandUtils.combineSimultaneous(CommandUtils.combineSequential(
-				new WaitTimeCommand(1000),
+		
+				new WaitTimeCommand(900),
 				new MoveRightCommand(io.robot),
-				new WaitTimeCommand(1000),
+				new WaitTimeCommand(100),
 				new MoveRightCommand(io.robot),
-				new WaitTimeCommand(1000),
+				new WaitTimeCommand(900),
 				new MoveRightCommand(io.robot),
-				new WaitTimeCommand(1000),
+				new WaitTimeCommand(900),
 				new MoveRightCommand(io.robot),
-				new WaitTimeCommand(1000)),new MonitorPositionCommand(io.robot));
+				new WaitTimeCommand(900),
+				new WaitTimeCommand(900),
+				new WaitTimeCommand(900),
+				new WaitTimeCommand(900),
+				new MoveUpCommand(io.robot),
+				new MoveLeftCommand(io.robot, 3),
+				new WaitTimeCommand(41)),new MonitorPositionCommand(io.robot));
+		
 	}
-
+	
 }
