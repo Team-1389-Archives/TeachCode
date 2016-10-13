cb5e516e84790ae416f6c3d5435394c877a7388a
Jonathan <Jonathan@10.208.67.47>
2016-10-13 15:57:21 -0400

    Ability to move in a sqaure direction. Description:Able to move with for loops

--- SlickLibSetup/src/userProgram/UserProgram.java
+++ SlickLibSetup/src/userProgram/UserProgram.java
@@ -1,30 +1,59 @@
 package userProgram;
 
-import commands.Command;
-import commands.CommandUtils;
-import commands.MonitorPositionCommand;
-import commands.MoveRightCommand;
-import commands.WaitTimeCommand;
+import java.util.ArrayList;
+import java.util.List;
+
+import commands.*;
 import layout.IOLayout;
 import programming.Program;
 
-public class UserProgram extends Program{
+public class UserProgram extends Program {
 	public UserProgram(IOLayout io) {
 		super(io);
 	}
 
 	@Override
 	public Command provideCommand() {
-		return CommandUtils.combineSimultaneous(CommandUtils.combineSequential(
-				new WaitTimeCommand(1000),
-				new MoveRightCommand(io.robot),
-				new WaitTimeCommand(1000),
-				new MoveRightCommand(io.robot),
-				new WaitTimeCommand(1000),
-				new MoveRightCommand(io.robot),
-				new WaitTimeCommand(1000),
-				new MoveRightCommand(io.robot),
-				new WaitTimeCommand(1000)),new MonitorPositionCommand(io.robot));
+		List<Command> listCommands = new ArrayList<Command>(); //compiling a mutiple commands into one thing aka, array
+		
+		for (int i = 0; i < 4; i++) { // a for command, first is your intial value, your limit, then your interval
+			Command pauseCommand = new WaitTimeCommand(1000);
+			Command rightmoveCommand = new MoveRightCommand(io.robot); // setting the command as a varibale to package into arrary
+			listCommands.add(rightmoveCommand); // add "right moveCommand" to the list
+			listCommands.add(pauseCommand);//see above
+			 
+		}
+		 for (int i = 0; i < 6; i++) { // a for command, first is your intial value, your limit, then your interval
+			Command pauseCommand1 = new WaitTimeCommand(1000);
+			Command downmoveCommand = new MoveDownCommand(io.robot);
+			listCommands.add(downmoveCommand);
+			listCommands.add(pauseCommand1);
+		 }
+		for (int i = 0; i < 4; i++) { 
+			Command leftmoveCommand = new MoveLeftCommand(io.robot);
+			listCommands.add(leftmoveCommand);
+			Command pauseCommand2 = new WaitTimeCommand(1000);
+			listCommands.add(pauseCommand2);
+			 }
+			 for (int i = 0; i < 4; i++) { 
+			Command upmoveCommand = new MoveUpCommand(io.robot);
+			listCommands.add(upmoveCommand);
+			Command pauseCommand3 = new WaitTimeCommand(1000);
+			listCommands.add(pauseCommand3); 
+		}
+		
+		Command command = new MonitorPositionCommand(io.robot);
+		listCommands.add(command);
+		Command[] arrayCommands = (Command[]) listCommands.toArray(new Command[listCommands.size()]);
+		return CommandUtils.combineSimultaneous(CommandUtils.combineSequential(arrayCommands));
+//this component basically packages everything, all commands, and executes them
 	}
+	
+	
 
+	/*
+	 * public static void main(String[] args) { int j=0; //int i=0; for (int
+	 * i=0; i >-6;i++) { i++; System.out.println("Current i is:"+i); j++; }
+	 * System.out.println("I have run the loop "+j+" times."); }
+	 */
 }
