8e0e37e6e6117d424f5fcf1d0b0389015cd3b301
325196 <325196@427-D213-10-205.mcpsmd.org>
2016-10-20 16:15:48 -0400

    Move in a squiggle

--- SlickLibSetup/src/userProgram/UserProgram.java
+++ SlickLibSetup/src/userProgram/UserProgram.java
@@ -4,9 +4,11 @@ import layout.IOLayout;
 import programming.Program;
 import userCommands.MoveForwardCommand;
 import userCommands.TurnRightCommand;
-
 import commands.Command;
 import commands.CommandUtils;
+import commands.MoveRightCommand;
+import commands.TurnLeftCommand;
+import commands.WaitTimeCommand;
 
 public class UserProgram extends Program{
 	public UserProgram(IOLayout io) 
@@ -16,11 +18,25 @@ public class UserProgram extends Program{
 public static int name = 0;
 	@Override
 	public Command provideCommand() {
-		Command C=CommandUtils.combineSequential(new MoveForwardCommand(io.robot), new TurnRightCommand(io.robot));
-				
-				
-				return CommandUtils.combineSequential(C,C,C,C);
-				
+//		Command C=CommandUtils.combineSequential(new MoveForwardCommand(io.robot), new TurnRightCommand(io.robot));
+//				return CommandUtils.combineSequential(C,C,C,C);
+		
+			Command M=CommandUtils.combineSequential(new MoveForwardCommand (io.robot),
+			new WaitTimeCommand (1000),
+			new TurnRightCommand (io.robot),
+			new WaitTimeCommand (1000),
+			new MoveForwardCommand (io.robot),
+			new WaitTimeCommand (1000),
+			new TurnRightCommand (io.robot),
+			new WaitTimeCommand (1000),
+			new MoveForwardCommand (io.robot),
+			new WaitTimeCommand (1000),
+			new TurnLeftCommand (io.robot),
+			new MoveForwardCommand (io.robot),
+			new WaitTimeCommand (1000),
+			new TurnRightCommand (io.robot));
+			
+			return CommandUtils.combineSequential (M,M,M,M);
 	
 			
 
